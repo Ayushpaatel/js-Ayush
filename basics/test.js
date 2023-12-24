f09d17bd24244b99bf6f833fa40700316c4031f2
Ayushpaatel <154582729+Ayushpaatel@users.noreply.github.com>
2023-12-24 11:10:06 +0000

    container config code

---
+++ basics/test.js
@@ -0,0 +1,11 @@
+const accountId = 14455544
+let accountEmail = "ayushpaatel207@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2// Not allowed
+accountEmail = "ayushpaatel201@gmail.com"
+accountPassword = "2121212"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
